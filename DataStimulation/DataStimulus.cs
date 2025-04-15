e109e8f36a8b1706f15f354dba7ada26273a25d1
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-04-15 17:12:20 +0100

    feat: implement `DataStimulus` in-memory data store to support CRUD operations

---
+++ DataStimulation/DataStimulus.cs
@@ -0,0 +1,15 @@
+﻿using StudentApi.Models;
+
+namespace StudentApi.DataStimulation
+{
+    public class DataStimulus
+    {
+        public static readonly List<Student> Students = new List<Student>
+        {
+            new Student {Id = 1, Name = "Ali Ahmed", Age = 20, Grade = 67 },
+            new Student {Id = 2, Name = "Fadi Khalil", Age = 17, Grade = 77 },
+            new Student {Id = 3, Name = "Ola Jaber", Age = 21, Grade = 54 },
+            new Student {Id = 4, Name = "Alia Maher", Age = 24, Grade = 86 }
+        };
+    }
+}
