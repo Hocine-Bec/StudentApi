794cf0dba762b91fb2d1be0f55e8b5594dc0dd26
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-04-15 16:40:46 +0100

    feat: implement Student model and RESTful StudentsController

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using StudentApi.DataStimulation;
+using StudentApi.Models;
+
+namespace StudentApi.Controllers
+{
+    [Route("api/Students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
+        {
+            return Ok(DataStimulus.Students);
+        }
+    }
+}
